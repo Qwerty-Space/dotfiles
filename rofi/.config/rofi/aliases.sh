9b63c635a4147009d676c5bd5ec35b849d94e473
Qwerty-Space <oliver_duce@live.co.uk>
2020-07-23 15:02:31 +0100

    better alias matching

--- rofi/.config/rofi/aliases.sh
+++ rofi/.config/rofi/aliases.sh
@@ -1,4 +1,6 @@
-alias | awk -F'[ =]' '{print $2}'
-alias tunnel='bash ~/scripts/tunnel.sh'
+#!/bin/zsh
+
+# alias | awk -F'[ =]' '{print $2}'
+print -rl -- ${(k)aliases} .zsh_aliases | grep -v .zsh_aliases
 
 source ~/.zsh_aliases
