c80d6c99c6a83b9e6bb320fe7fd7da786965a6c0
Qwerty-Space <oliver_duce@live.co.uk>
2020-12-18 20:59:21 +0000

    i want to commit changes

--- scripts/scripts/safe_wallpaper.sh
+++ scripts/scripts/safe_wallpaper.sh
@@ -2,7 +2,7 @@
 # create and delete file
 
 filepath=/run/user/1000/wallpaper_safe
-text="If this file exists, the slideshow will be safe"
+text="If this file exists, the slideshow will be sfw"
 
 if [ -e "$filepath" ]; then
     rm "$filepath"
