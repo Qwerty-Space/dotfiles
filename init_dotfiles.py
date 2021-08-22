da63d5496d56c05e6cab2c15d117e94f6cc895d9
Qwerty-Space <oliver_duce@live.co.uk>
2021-08-22 15:14:30 +0100

    make better

---
+++ init_dotfiles.py
@@ -0,0 +1,75 @@
+import os
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(description="Initialise dotfiles by creating the directory struture")
+parser.add_argument("-d", "--dry", default=False, action="store_const", const=True)
+parser.add_argument("-p", "--path", default=".", action="store")
+parser.add_argument("-t", "--target", default="..", action="store")
+
+args = parser.parse_args()
+
+
+def create_dirs(packages):
+    directories = set()
+    asked = False
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in dirs:
+            if not asked:
+                asked = True
+                strip = input(
+                    "\n" + os.path.join(root, name) +
+                    "\nHow many directories deep are the required files?  (default: 2):  "
+                ) or 2
+                strip = int(strip)
+
+            # Split the path up and strip the first few directories
+            dir = (os.path.join(root, name)).split("/")[strip:]
+            # join the path back together
+            if not dir:
+                continue
+            print(dir)
+            directories.add(os.path.join(*dir))
+
+    for name in directories:
+        name_dir = os.path.split(name)
+        dir = os.path.join(target, name.lstrip("/"))#.replace("dot-", ".")
+        if args.dry:
+            print(dir)
+        else:
+            os.makedirs(dir, exist_ok=True)
+
+
+def stow_dirs(packages):
+    # directories = os.listdir(args.path)
+    if args.dry:
+        print("Directories to stow:  ", packages)
+        return
+
+    subprocess.run(["stow", "-t", target, path])
+
+
+def main():
+    packages = os.listdir(path)
+
+    create = input("Create directories?  [Y/n]:  ").casefold() or "y"
+    if create.startswith("y"):
+        create_dirs(packages)
+
+    stow = input("Stow dotfiles?  [Y/n]:  ").casefold() or "y"
+    if stow.startswith("y"):
+        stow_dirs(packages)
+
+
+if __name__ == "__main__":
+    path = os.path.expanduser(os.path.expandvars(args.path))
+    target = os.path.expanduser(os.path.expandvars(args.target))
+    print("Settings")
+    print("Dry run: ", args.dry)
+    print("Dotfiles path: ", args.path)
+    print("Target: ", target)
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit
---
+++ install_packages.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+shopt -s nocasematch
+
+base="ranger atool stow python figlet zsh zsh-syntax-highlighting ffmpeg neofetch \
+htop lynx mediainfo vim ncdu openssh syncthing tmux wget youtube-dl"
+
+base_aur="lsd-git"
+
+gui="urxvt rofi rofi-calc networkmanager network-manager-applet firefox thunderbird \
+herbstluftwm polybar blueman flameshot dunst udiskie mpv redshift xclip slop \
+imagemagick capitaine-cursors arc-icon-theme code feh filezilla noto-fonts \
+noto-fonts-cjk noto-fonts-emoji noto-fonts-extra numlockx obs-studio pavucontrol \
+xsensors pulseaudio pulseaudio-alsa pulseaudio-bluetooth zenity"
+
+gui_aur="dragon-drag-and-drop i3lock-color tauon-music-box matcha-gtk-theme logiops \
+liri-calculator neovim-nightly-bin nerd-fonts-complete obs qimgv-git qmk-git syncplay-git"
+
+gaming="discord-canary steam"
+
+gaming_aur="multimc5"
+
+extras="convmv hexchat kcolorchooser mkvtoolnix-gui mkvtoolnix-cli"
+
+extras_aur="lazygit-git mp3tag megatools-bin mumble python-bdfr"
+
+
+echo -en "\nInstall paru? [Y/n]:  "
+read -n 1 install_paru
+install_paru=${install_paru:-y}
+
+
+echo -e "\n$base"
+echo -en "\nInstall Base packages? [Y/n]:  "
+read -n 1 install_base
+install_base=${install_base:-y}
+
+if [[ $install_paru == "y" ]]; then
+    echo -e "\n$base_aur"
+    echo -en "\nInstall Base AUR packages? [Y/n]:  "
+    read -n 1 install_base_aur
+    install_base_aur=${install_base_aur:-y}
+fi
+
+echo -e "\n$gui"
+echo -en "\nInstall GUI packages? [Y/n]:  "
+read -n 1 install_gui
+install_gui=${install_gui:-y}
+
+if [[ $install_paru == "y" ]]; then
+    echo -e "\n$gui_aur"
+    echo -en "\nInstall GUI AUR packages? [Y/n]:  "
+    read -n 1 install_gui_aur
+    install_gui_aur=${install_gui_aur:-y}
+fi
+
+echo -e "\n$gaming"
+echo -en "\nInstall gaming packages? [y/N]:  "
+read -n 1 install_gaming
+install_gaming=${install_gaming:-n}
+
+if [[ $install_paru == "y" ]]; then
+    echo -e "\n$gaming_aur"
+    echo -en "\nInstall gaming AUR packages? [y/N]:  "
+    read -n 1 install_gaming_aur
+    install_gaming_aur=${install_gaming_aur:-n}
+fi
+
+echo -e "\n$extras"
+echo -en "\nInstall extra packages? [y/N]:  "
+read -n 1 install_extras
+install_extras=${install_extras:-n}
+
+if [[ $install_paru == "y" ]]; then
+    echo -e "\n$extras_aur"
+    echo -en "\nInstall extra AUR packages? [y/N]:  "
+    read -n 1 install_extras_aur
+    install_extras_aur=${install_extras_aur:-n}
+fi
+
+
+if [[ $install_base == "y" ]]; then
+    packages+="$base "
+fi
+
+if [[ $install_gui == "y" ]]; then
+    packages+="$gui "
+fi
+
+if [[ $install_gaming == "y" ]]; then
+    packages+="$gaming "
+fi
+
+if [[ $install_extras == "y" ]]; then
+    packages+="$extras "
+fi
+
+
+if [[ $install_paru == "n" ]]; then
+    echo "Packages: $packages"
+    sudo pacman -Syu $packages
+    exit
+fi
+
+
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+if [[ $install_base_aur == "y" ]]; then
+    packages+="$base_aur "
+fi
+
+if [[ $install_gui_aur == "y" ]]; then
+    packages+="$gui_aur "
+fi
+
+if [[ $install_gaming_aur == "y" ]]; then
+    packages+="$gaming_aur "
+fi
+
+if [[ $install_extras_aur == "y" ]]; then
+    packages+="$extras_aur "
+fi
+
+echo "Packages: $packages"
+paru -Syu $packages
+
