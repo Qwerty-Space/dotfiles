4781027e576e5892700d1bddfa7da67ceaa8e679
Qwerty-Space <oliver_duce@live.co.uk>
2022-04-08 10:48:02 +0100

    thanks sam

--- home/nixpkgs/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
+++ home/nixpkgs/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
@@ -5,16 +5,16 @@ self: super:
     patches = (oldAttrs.patches or []) ++ [
       ./tdesktop-udf-patches/always_delete_for_everyone.patch
       ./tdesktop-udf-patches/always_clear_history_for_everyone.patch
-      ./tdesktop-udf-patches/always_pin_without_notify.patch
+      #./tdesktop-udf-patches/always_pin_without_notify.patch
       ./tdesktop-udf-patches/always_send_as_photo_or_album.patch
-      ./tdesktop-udf-patches/use_tagbot_for_gifs_search.patch
+      #./tdesktop-udf-patches/use_tagbot_for_gifs_search.patch
     ];
 
     postFixup = ''
       wrapProgram $out/bin/telegram-desktop \
         "''${gappsWrapperArgs[@]}" \
         "''${qtWrapperArgs[@]}" \
-        --set XDG_CURRENT_DESKTOP Unity \
+        --set XDG_CURRENT_DESKTOP KDE \
         --set QT_XCB_GL_INTEGRATION none \
         --prefix XDG_DATA_DIRS : /usr/share \
         --prefix XCURSOR_PATH : /usr/share/icons
