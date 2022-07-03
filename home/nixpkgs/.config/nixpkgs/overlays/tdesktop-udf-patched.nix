7a23134f0136cde4c25c46c1a04df40d9745b669
Qwerty-Space <oliver_duce@live.co.uk>
2022-07-03 16:58:08 +0100

    other stuff

--- home/nixpkgs/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
+++ home/nixpkgs/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
@@ -5,19 +5,20 @@ self: super:
     patches = (oldAttrs.patches or []) ++ [
       ./tdesktop-udf-patches/always_delete_for_everyone.patch
       ./tdesktop-udf-patches/always_clear_history_for_everyone.patch
-      #./tdesktop-udf-patches/always_pin_without_notify.patch
+      ./tdesktop-udf-patches/always_pin_without_notify.patch
       ./tdesktop-udf-patches/always_send_as_photo_or_album.patch
-      #./tdesktop-udf-patches/use_tagbot_for_gifs_search.patch
+      ./tdesktop-udf-patches/use_tagbot_for_gifs_search.patch
     ];
 
     postFixup = ''
       wrapProgram $out/bin/telegram-desktop \
         "''${gappsWrapperArgs[@]}" \
         "''${qtWrapperArgs[@]}" \
-        --set XDG_CURRENT_DESKTOP KDE \
+        --set XDG_CURRENT_DESKTOP Unity \
         --set QT_XCB_GL_INTEGRATION none \
         --prefix XDG_DATA_DIRS : /usr/share \
-        --prefix XCURSOR_PATH : /usr/share/icons
+        --prefix XCURSOR_PATH : /usr/share/icons \
+        --prefix LD_LIBRARY_PATH : ${self.xorg.libXcursor}/lib
     '';
   });
 }
