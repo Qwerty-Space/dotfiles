73089db145ad2d2536db4ef8229e612cbc8c99e6
Qwerty-Space <oliver_duce@live.co.uk>
2025-01-04 02:24:30 +0000

    dumb

--- home/tl/scripts/tl/tl.py
+++ home/tl/scripts/tl/tl.py
@@ -10,6 +10,7 @@ except IndexError:
 
 script_dir = path.dirname(path.realpath(__file__))
 client = TelegramClient(path.join(script_dir, session), 6, "eb06d4abfb49dc3eeb1aeb98ae0f581e").start()
+client = TelegramClient(path.join(script_dir, session), 344523, "fa96fd639328fbc9aa7c16693a05a380").start()
 c = client
 
 print(client.get_me().first_name)
