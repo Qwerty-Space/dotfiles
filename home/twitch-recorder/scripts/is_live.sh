5c55cdead77a5f083076ce46e30c28d671dce6d4
Qwerty-Space <oliver_duce@live.co.uk>
2025-05-10 12:36:18 +0100

    move compression to its own script
    this means if a stream goes down for a few minutes, i won't miss it

--- home/twitch-recorder/scripts/is_live.sh
+++ home/twitch-recorder/scripts/is_live.sh
@@ -5,36 +5,33 @@ function is_live() {
   local channel_name=$1 # Store the channel name passed as an argument
   # store the channel link
   local channel_link="https://www.twitch.tv/$channel_name"
-  # Fetch the HTML content of the Twitch channel page using curl
-  local content=$(curl -s $channel_link)
-  # Extract the title
-  local description=$(echo "$content" | sed -E 's/.*"description":"([^"]+)".*/\1/')
-  # Convert Unicode escape sequences to UTF-8 (e.g., \u003e -> >)
-  local utf8_description=$(echo -e "$description" | sed 's/\\u\([0-9a-fA-F]\{4\}\)/\\x\1/g' | while read -r line; do echo -e "$line"; done)
-  # Convert to a safe filename
-  local title=$(echo "$utf8_description" | tr -cd '[:alnum:][:space:]._-')
-  # Get today's date
-  local now=$(date -u "+%Y-%m-%d")
-  # Combine title and date into filename
-  local filename="$now $title.mp4"
 
   # Check if channel is live using streamlink
   streamlink --stream-url $channel_link
 
   # If streamlink outputs 0, then the channel is live
-  if [ $? -eq 0 ]; then
-    echo "$channel_name is live" # Print a message if the channel is live
-    dunstify -a "$USER@$(cat /etc/hostname)" "Recording $channel_name's stream"
-    echo "Steam Title:  $utf8_description"
-    # Download the stream, and compress the video
-    streamlink --twitch-disable-ads -o "Uncompressed $filename" $channel_link best
-    ffmpeg -i "Uncompressed $filename" -r 30 -b:v 1.5M "$filename"
-    rm "Uncompressed $filename"
-    # yt-dlp -o "$now $filename.%(ext)s" --postprocessor-args "-r 30 -b:v 2M" $channel_link
-    dunstify -a "$USER@$(cat /etc/hostname)" "$channel_name's stream downloaded"
-  else
+  if [ $? -ne 0 ]; then
     echo "$channel_name is not live :(" # Print a message if the channel is not live
+    exit 0
   fi
+
+  # Fetch the title using streamlink
+  local pre_title=$(streamlink --json $channel_link | jq -r '.metadata.title')
+  # Convert Unicode escape sequences to UTF-8 (e.g., \u003e -> >)
+  local utf8_title=$(echo -e "$pre_title" | sed 's/\\u\([0-9a-fA-F]\{4\}\)/\\x\1/g' | while read -r line; do echo -e "$line"; done)
+  # Convert to a safe filename
+  local title=$(echo "$utf8_title" | tr -cd '[:alnum:][:space:]._-')
+  # Get today's date
+  local now=$(date -u "+%Y-%m-%d %H-%M-%S")
+  # Combine title and date into filename
+  local filename="$now $title.mp4"
+
+  echo "$channel_name is live" # Print a message if the channel is live
+  dunstify -a "$USER@$(cat /etc/hostname)" "Recording $channel_name's stream"
+  echo "Steam Title:  $filename"
+  # Download the stream, and compress the video
+  streamlink --twitch-disable-ads -o "Uncompressed $filename" $channel_link best
+  dunstify -a "$USER@$(cat /etc/hostname)" "$channel_name's stream downloaded"
 }
 
 # Check if the user provided a channel name as an argument
