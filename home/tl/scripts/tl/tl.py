77b5d3cec3efbb43524eb9bacacf08fec7d87c21
Qwerty-Space <oliver_duce@live.co.uk>
2025-08-03 19:45:50 +0100

    no longer require python -i

--- home/tl/scripts/tl/tl.py
+++ home/tl/scripts/tl/tl.py
@@ -1,16 +1,27 @@
 import sys
+import code
 from os import path
 from telethon import TelegramClient, custom, events, sync, functions as f, types as t
 from telethon.tl.custom import Button
+
+# set the script dir and import config
+script_dir = path.dirname(path.realpath(__file__))
+sys.path.append(script_dir)
 import config
 
-try:
-    session = sys.argv[1]
-except IndexError:
-    session = "default"
+def main():
+    try:
+        session = sys.argv[1]
+    except IndexError:
+        session = "default"
 
-script_dir = path.dirname(path.realpath(__file__))
-client = TelegramClient(path.join(script_dir, session), config.id, config.hash).start()
-c = client
 
-print(client.get_me().first_name)
+    client = TelegramClient(path.join(
+        script_dir, session), config.id, config.hash).start()
+
+    code.interact(banner=f"Logged in as:  {client.get_me().first_name} ",
+                  local=locals())
+
+if __name__ == "__main__":
+    main()
+
