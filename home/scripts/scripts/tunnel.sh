4c06775b51cb7c35a8adeb6f11da870cb1d97977
Qwerty-Space <oliver_duce@live.co.uk>
2025-08-03 18:26:48 +0100

    rip zeus ;-;

--- home/scripts/scripts/tunnel.sh
+++ home/scripts/scripts/tunnel.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # ssh tunnel
 
-ssh -D 50203 -l qwertyspace zeus.feralhosting.com -f -N
+ssh -D 50203 -l qwertyspace gegenees -f -N
+ssh -D 50204 -l qwerty shintaku -f -N
+ssh -D 50205 -l boba blueberry -f -N
