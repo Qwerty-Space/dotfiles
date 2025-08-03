c360994f290f52a33080c3fb79f1435b224d2cae
Qwerty-Space <oliver_duce@live.co.uk>
2025-08-03 18:33:45 +0100

    improve script

--- home/tl/scripts/tl/tl.py
+++ home/tl/scripts/tl/tl.py
@@ -2,6 +2,7 @@ import sys
 from os import path
 from telethon import TelegramClient, custom, events, sync, functions as f, types as t
 from telethon.tl.custom import Button
+import config
 
 try:
     session = sys.argv[1]
@@ -9,10 +10,7 @@ except IndexError:
     session = "default"
 
 script_dir = path.dirname(path.realpath(__file__))
-<<<<<<< HEAD
-client = TelegramClient(path.join(script_dir, session), 6, "eb06d4abfb49dc3eeb1aeb98ae0f581e").start()
-=======
->>>>>>> ef93267 (add mullvad and other stuff)
+client = TelegramClient(path.join(script_dir, session), config.id, config.hash).start()
 c = client
 
 print(client.get_me().first_name)
