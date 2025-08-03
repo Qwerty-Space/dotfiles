f6bbc1d5fb1ec38de9edee3cbeefe4c4d274d613
Qwerty-Space <oliver_duce@live.co.uk>
2025-08-03 18:25:44 +0100

    add mullvad and other stuff

--- home/tl/scripts/tl/tl.py
+++ home/tl/scripts/tl/tl.py
@@ -9,7 +9,10 @@ except IndexError:
     session = "default"
 
 script_dir = path.dirname(path.realpath(__file__))
+<<<<<<< HEAD
 client = TelegramClient(path.join(script_dir, session), 6, "eb06d4abfb49dc3eeb1aeb98ae0f581e").start()
+=======
+>>>>>>> ef93267 (add mullvad and other stuff)
 c = client
 
 print(client.get_me().first_name)
--- home/twitch-recorder/scripts/compress_streams.sh
+++ home/twitch-recorder/scripts/compress_streams.sh
@@ -2,22 +2,21 @@
 
 # Function to check if a Twitch channel is live
 function compress() {
-  # find $1 -type f \( -name "Uncompressed*.mp4" -o -name "Uncompressed*.webm" \) -execdir sh -c '
-  #   file="{}"
-  #   echo compressing: {}
-  #   output_file="${file//Uncompressed /}"
-  #   output_file="${output_file//.webm/.mp4}"
-  #   echo output: $output_file
-  #   # ffmpeg -i "{}" -r 30 -b:v 1.5M "$output_file" && rm "{}"
-  # ' \;
-  fd --type f --threads=1 "^Uncompressed.*\.(mp4|webm)$" $1 --exec sh -c '
+  find $1 -type f \( -name "Uncompressed*.mp4" -o -name "Uncompressed*.webm" \) -execdir sh -c '
+    file="{}"
     echo compressing: {}
-    file="{.}"
     output_file="${file//Uncompressed /}"
-    # output_file="${output_file//.webm/.mp4}"
-    echo output: $output_file.mp4
-    ffmpeg -i "{}" -r 30 -b:v 1.5M "$output_file.mp4" && rm "{}"
-  '
+    output_file="${output_file//.webm/.mp4}"
+    echo output: $output_file
+    ffmpeg -i "{}" -r 30 -b:v 1.5M "$output_file" && rm "{}"
+  ' \;
+  # fd --type f --threads=1 "^Uncompressed.*\.(mp4|webm)$" $1 --exec sh -c '
+  #   echo compressing: {}
+  #   file="{.}"
+  #   output_file="${file//Uncompressed /}"
+  #   echo output: $output_file.mp4
+  #   ffmpeg -i "{}" -r 30 -b:v 1.5M "$output_file.mp4" && rm "{}"
+  # '
 }
 
 # Check if the user provided a path as an argument
